64546bc0738a456c1b19faa0ca4decb22eb12ba1
Satya Prakash <165584094+Satyaprakash666@users.noreply.github.com>
2026-02-25 00:57:29 +0530

    Update firebase.js

--- static/js/firebase.js
+++ static/js/firebase.js
@@ -1,12 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBEPh61ad4j9qogxH457h79NE9AQJD_tUo",
-    authDomain: "helpforfarmer-c717e.firebaseapp.com",
-    projectId: "helpforfarmer-c717e",
-    storageBucket: "helpforfarmer-c717e.firebasestorage.app",
-    messagingSenderId: "645087836594",
-    appId: "1:645087836594:web:ce04cb38c529d5fe7d8cab",
-    measurementId: "G-MSVG3D5XTD"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
